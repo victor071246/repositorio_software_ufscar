ec90685431a66ab8402064b1e873ec18f31b1ead
Victor <daderiovictorfelipe@gmail.com>
2025-11-16 19:00:25 -0300

    Atualizando dependencias para build 1

--- frontend/src/app/pages/visualizar_usuario/page.tsx
+++ frontend/src/app/pages/visualizar_usuario/page.tsx
@@ -1,4 +1,7 @@
 'use client';
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+
 
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
